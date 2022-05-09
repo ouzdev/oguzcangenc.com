a19746edef688c3e66b6d366a3689d7362a4fe5a
Oğuzcan Genç <oguzcangencc@hotmail.com>
2022-05-09 21:09:20 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//Enable tooltips everywhere
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+    new GitHubCalendar("#github-graph", "ouzdev", { responsive: true });
+    GitHubActivity.feed({ username: "ouzdev", selector: "#ghfeed" });
